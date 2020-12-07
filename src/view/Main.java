19384a5f22f3eb9cca42ca10824cde591f8077bc
julianaacruz <juliana201380cc@gmail.com>
2020-12-07 08:48:22 -0500

    Archivo creado con Main

---
+++ src/view/Main.java
@@ -0,0 +1,30 @@
+package view;
+
+import processing.core.PApplet;
+
+public class Main  extends PApplet{
+
+	public static void main(String[] args) {
+		PApplet.main(Main.class.getName());
+
+	}
+	
+	public void settings() {
+		size(768,458);
+	}
+	
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+	}
+	
+	
+	public void keyPressed() {
+	}
+
+	public void mousePressed() {
+	}
+}
