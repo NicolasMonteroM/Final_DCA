17256eb693179c66eabe0273589337a447464e49
julianaacruz <juliana201380cc@gmail.com>
2020-12-07 16:45:11 -0500

    Botones de pantalla de registro

--- src/view/Main.java
+++ src/view/Main.java
@@ -3,8 +3,7 @@ package view;
 import processing.core.PApplet;
 import processing.core.PFont;
 
-public class Main  extends PApplet{
-	
+public class Main extends PApplet {
 
 	PFont font;
 	private Register register;
@@ -14,49 +13,75 @@ public class Main  extends PApplet{
 		PApplet.main(Main.class.getName());
 
 	}
-	
+
 	public void settings() {
-		size(768,458);
+		size(768, 458);
 	}
-	
-	
+
 	public void setup() {
 		screen = "register";
-		font = createFont("Anonymous Pro Bold",12);
-		register= new Register(this);
-		register.drawTextfield(); // Don't move from setup
-		
-	
+		font = createFont("Anonymous Pro Bold", 12);
+		register = new Register(this);
 
 	}
-	
+
 	public void draw() {
 		background(0);
 		textFont(font);
 
-		//Screens
+		// Screens
+		System.out.println(screen);  
 		
-		switch(screen) {
-		
-		  case "register":
-		register.drawScreen();
-		fill(240);
-		text("LEADERBOARD", 350, 385);
-		
-		break;
-		
-		
+		switch (screen) { 
+
+		case "register":
+			register.drawScreen();
+			screen = register.changeScreen();
+			fill(240);
+			text("LEADERBOARD", 350, 385);
+
+			break;
+
+		case "level 1":
+			register.getCp5().hide();
+			background(255);
+			fill(0);
+			text("Nivel 1", width/2, height/2);
+
+			break;
+			
+		case "level 2":
+			background(255);
+			fill(0);
+			text("Nivel 2", width/2, height/2);
+
+			break;
+			
+		case "level 3":
+			background(0);
+			fill(255);
+			text("Nivel 3", width/2, height/2);
+
+			break;
+			
+		case "leaderboard":
+			register.getCp5().hide();
+			background(0);
+			fill(255);
+			text("LEADERBOARD", width/2, height/2);
+
+			break;
+
 		}
 
-		
-		System.out.println(mouseX + " - " + mouseY); // Mouse position on console
+		// System.out.println(mouseX + " - " + mouseY); // Mouse position on console
 
 	}
-	
-	
+
 	public void keyPressed() {
 	}
 
 	public void mousePressed() {
+		register.registerNewPlayer();
 	}
 }
